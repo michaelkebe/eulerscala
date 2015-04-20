f567f320116f1268362b2762ec21d88b46639c97
Michael Kebe <michael.kebe@gmail.com>
2015-04-20 18:36:15 +0200

    Problem 3

---
+++ src/p003/Main.scala
@@ -0,0 +1,17 @@
+package p003
+
+object Main {
+
+  def primeFactorsOf(n: Long): List[Long] = {
+    val foobar = (2L to math.sqrt(n).toLong).find(n % _ == 0)
+    foobar match {
+      case Some(x) => x :: primeFactorsOf(n / x)
+      case None => List(n)
+    }
+  }
+
+  def main(args: Array[String]) {
+    val answer = primeFactorsOf(600851475143L).max
+    println(answer)
+  }
+}
