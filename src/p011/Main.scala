307e41aa6685bca689888963868de25eab2038e9
Michael Kebe <michael.kebe@gmail.com>
2015-04-22 08:14:14 +0200

    Use lineSeparator instead of \n

--- src/p011/Main.scala
+++ src/p011/Main.scala
@@ -23,7 +23,7 @@ object Main {
       |20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
       |20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
       |01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"""
-      .stripMargin.split("\n").toList.map(_.split(" ").map(_.toInt).toList)
+      .stripMargin.split(util.Properties.lineSeparator).toList.map(_.split(" ").map(_.toInt).toList)
 
   private val numberOfAdjacentNumbers = 4
   private val gridSize = grid.length
