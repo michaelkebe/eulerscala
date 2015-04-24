24dd1eac14e2602ebcc0f3dcbec101917491e419
Michael Kebe <michael.kebe@gmail.com>
2015-04-24 15:30:26 +0200

    Faster solution for Problem 5

--- src/p005/Main.scala
+++ src/p005/Main.scala
@@ -1,9 +1,12 @@
 package p005
 
 object Main {
+  private val divisorsToCheck = 11 to 20
+  private def isDivisibleBy1To20(n: Int) = divisorsToCheck.forall(n % _ == 0)
+
   def main(args: Array[String]) {
-    val answer = Iterator.from(1)
-      .find(n => (1 to 20).forall(n % _ == 0)).get
+    val answer = Iterator.from(start = 20, step = 20)
+      .find(isDivisibleBy1To20).get
     println(answer)
   }
 }
