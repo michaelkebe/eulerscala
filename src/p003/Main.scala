d25cf03030eadf3f2ca4502ebccecba9ce0d0a32
Michael Kebe <michael.kebe@gmail.com>
2015-04-24 09:15:06 +0200

    renamed variable

--- src/p003/Main.scala
+++ src/p003/Main.scala
@@ -2,8 +2,8 @@ package p003
 
 object Main {
   def primeFactorsOf(n: Long): List[Long] = {
-    val foobar = (2L to math.sqrt(n).toLong).find(n % _ == 0)
-    foobar match {
+    val factor = (2L to math.sqrt(n).toLong).find(n % _ == 0)
+    factor match {
       case Some(x) => x :: primeFactorsOf(n / x)
       case None => List(n)
     }
