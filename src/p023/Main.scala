aa9f288384d3e2d62128ab14844b8664dd00a74d
Michael Kebe <michael.kebe@gmail.com>
2015-04-27 15:19:05 +0200

    Problem 23

---
+++ src/p023/Main.scala
@@ -0,0 +1,25 @@
+package p023
+
+object Main {
+  private val limit = 28123
+
+  private def divisorsOf(n: Int) = {
+    (1 to n / 2).filter(n % _ == 0)
+  }
+
+  private val isAbudantCache: collection.mutable.Map[Int, Boolean] = collection.mutable.Map()
+  private def isAbudant(n: Int) = {
+    isAbudantCache.getOrElseUpdate(n, divisorsOf(n).sum > n)
+  }
+
+  private def canBeWrittenAsSumOfTwoAbudantNumbers(n: Int): Boolean = {
+    (1 to n).exists { i =>
+      isAbudant(i) && isAbudant(n - i)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val answer = (1 to limit).filter(!canBeWrittenAsSumOfTwoAbudantNumbers(_)).sum
+    println(answer)
+  }
+}
