a3baa8095eb240521fbe0e7fda6e4235d06749a0
Michael Kebe <michael.kebe@gmail.com>
2015-04-27 15:33:23 +0200

    Problem 25

---
+++ src/p025/Main.scala
@@ -0,0 +1,15 @@
+package p025
+
+object Main {
+  val fibonacciNumbers = {
+    def fibonacci(a: BigInt, b: BigInt): Stream[BigInt] = {
+      a #:: fibonacci(b, a + b)
+    }
+    fibonacci(0, 1)
+  }
+
+  def main(args: Array[String]) {
+    val answer = Iterator.from(0).find(fibonacciNumbers(_).toString().length == 1000).get
+    println(answer)
+  }
+}
