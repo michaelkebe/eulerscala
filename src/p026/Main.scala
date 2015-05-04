dc1862768bd115d34565ee41a64619b547bbd395
Michael Kebe <michael.kebe@gmail.com>
2015-05-04 10:02:37 +0200

    Problem 26

---
+++ src/p026/Main.scala
@@ -0,0 +1,16 @@
+package p026
+
+object Main {
+
+  def lengthOfCycle(n: Int) = {
+    if (n % 2 == 0 || n % 5 == 0) 0
+    else Iterator.iterate(1) { i =>
+      (i * 10) % n
+    }.drop(1).takeWhile(_ != 1).length + 1
+  }
+
+  def main(args: Array[String]) {
+    val answer = (2 until 1000).maxBy(lengthOfCycle)
+    println(answer)
+  }
+}
